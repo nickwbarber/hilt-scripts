0b8f4eb663da6db8bd4920fd5f972828422490ba
Nick Barber <nickwbarber@gmail.com>
2017-12-07 17:19:16 -0600

    Update paths

--- sentence_corrector.py
+++ sentence_corrector.py
@@ -4,63 +4,72 @@ import os
 import gatenlp
 
 
-conversations_dir = "/home/nick/hilt/pes/conversations"
+#TODO: make this a CLI program
 
-annotation_file_paths = [
-    os.path.join(root, f)
-    for root, dirs, files in os.walk(conversations_dir)
-    for f in files
-    if f.lower().endswith("pes_3_consensus.xml")
+conversations_dirs = [
+    "/home/nick/hilt/pes/consensus_files_cleaned",
+    "/home/nick/hilt/pes/consensus_files_with_tags",
 ]
+# conversations_dir = "/home/nick/test/gate/evita"
 
-for annotation_file_path in annotation_file_paths:
+for conversations_dir in conversations_dirs:
 
-    annotation_file = gatenlp.AnnotationFile(annotation_file_path)
-    annotations = annotation_file.annotations
-    sentences = (
-        annotation
-        for annotation in annotations
-        if annotation.type.lower() == "sentence"
-    )
-    sentences = sorted(
-        sentences,
-        key=lambda x: x.start_node
-    )
+    annotation_file_paths = [
+        os.path.join(root, f)
+        for root, dirs, files in os.walk(conversations_dir)
+        for f in files
+        if f.lower().endswith("pes_3_consensus.xml")
+    ]
 
-    gatenlp.dlink(sentences)
+    for annotation_file_path in annotation_file_paths:
 
-    current_sentence = sentences[0]
-    while True:
-        if len(current_sentence) <= 2:
-            # gatenlp.unlink(current_sentence)
-            current_sentence.delete()
+        annotation_file = gatenlp.AnnotationFile(annotation_file_path)
+        annotations = annotation_file.annotations
+        sentences = (
+            annotation
+            for annotation in annotations
+            if annotation.type.lower() == "sentence"
+        )
+        sentences = sorted(
+            sentences,
+            key=lambda x: x.start_node
+        )
 
-            if current_sentence.previous:
-                previous_sentence = current_sentence.previous
-                previous_sentence.end_node = (
-                    previous_sentence.end_node
-                    + len(current_sentence)
-                )
-            elif current_sentence.next:
-                next_sentence = current_sentence.next
-                next_sentence.end_node = (
-                    next_sentence.end_node
-                    + len(current_sentence)
-                )
+        gatenlp.dlink(sentences)
 
-        current_sentence = current_sentence.next
-        if not current_sentence:
-            break
+        current_sentence = sentences[0]
+        while True:
+            if len(current_sentence) <= 2:
 
-    chars = set()
-    current_sentence = sentences[0]
-    while True:
-        if len(current_sentence) <= 2:
-            # print(sentence)
-            chars.add(current_sentence.text)
-        current_sentence = current_sentence.next
-        if not current_sentence:
-            break
+                if current_sentence.previous:
+                    previous_sentence = current_sentence.previous
+                    previous_sentence.end_node = current_sentence.end_node
+                    # previous_sentence.end_node = (
+                        # previous_sentence.end_node
+                        # + len(current_sentence)
+                    # )
+                elif current_sentence.next:
+                    next_sentence = current_sentence.next
+                    previous_sentence.start_node = current_sentence.start_node
+                    # next_sentence.end_node = (
+                        # next_sentence.end_node
+                        # + len(current_sentence)
+                    # )
+                current_sentence.delete()
+                # gatenlp.unlink(current_sentence)
 
-    # continue
-    annotation_file.save_changes()
+            current_sentence = current_sentence.next
+            if not current_sentence:
+                break
+
+        chars = set()
+        current_sentence = sentences[0]
+        while True:
+            if len(current_sentence) <= 2:
+                # print(sentence)
+                chars.add(current_sentence.text)
+            current_sentence = current_sentence.next
+            if not current_sentence:
+                break
+
+        annotation_file.save_changes()
--- speaker_tagger.py
+++ speaker_tagger.py
@@ -4,39 +4,46 @@ import os
 import gatenlp
 
 
-conversations_dir = "/home/nick/hilt/pes/conversations"
+#TODO: make this a CLI program
 
-annotation_file_paths = [
-    os.path.join(root, f)
-    for root, dirs, files in os.walk(conversations_dir)
-    for f in files
-    if f.lower().endswith("pes_3_consensus.xml")
+conversations_dirs = [
+    "/home/nick/hilt/pes/consensus_files_cleaned",
+    "/home/nick/hilt/pes/consensus_files_with_tags",
 ]
+# conversations_dir = "/home/nick/test/gate/evita"
 
-for annotation_file_path in annotation_file_paths:
+for conversations_dir in conversations_dirs:
 
-    annotation_file = gatenlp.AnnotationFile(annotation_file_path)
-    annotations = annotation_file.annotations
-    sentences = (
-        annotation
-        for annotation in annotations
-        if annotation.type.lower() == "sentence"
-    )
+    annotation_file_paths = [
+        os.path.join(root, f)
+        for root, dirs, files in os.walk(conversations_dir)
+        for f in files
+        if f.lower().endswith("pes_3_consensus.xml")
+    ]
 
-    sentences = sorted(
-        sentences,
-        key=lambda x: x.start_node
-    )
+    for annotation_file_path in annotation_file_paths:
 
-    speaker_tag = "None"
-    for sentence in sentences:
-        text = sentence.text
-        if ".wav" in text:
-            sentence.add_feature("Speaker", "None", overwrite=True)
-            continue
-        if ":" in text:
-            speaker_tag = text.split(":")[0]
-        sentence.add_feature("Speaker", speaker_tag, overwrite=True)
+        annotation_file = gatenlp.AnnotationFile(annotation_file_path)
+        annotations = annotation_file.annotations
+        sentences = (
+            annotation
+            for annotation in annotations
+            if annotation.type.lower() == "sentence"
+        )
 
-    continue
-    # annotation_file.save_changes()
+        sentences = sorted(
+            sentences,
+            key=lambda x: x.start_node
+        )
+
+        speaker_tag = "None"
+        for sentence in sentences:
+            text = sentence.text
+            if ".wav" in text:
+                sentence.add_feature("Speaker", "None", overwrite=True)
+                continue
+            if ":" in text:
+                speaker_tag = text.split(":")[0]
+            sentence.add_feature("Speaker", speaker_tag, overwrite=True)
+
+        annotation_file.save_changes()
